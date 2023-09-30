c7b24686053aed54b0d5dabd0b58b1a6a51185df
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-09-30 15:11:01 -0300

    Adicionando sons nas teclas.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.body.addEventListener("keyup", (event) => {
-  console.log(event.code);
+  playSound(event.code.toLowerCase());
 });
+
+function playSound(sound) {
+  let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElemento = document.querySelector(`div[data-key="${sound}"]`);
+
+  if (audioElement) {
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
+
+  if (keyElemento) {
+    keyElemento.classList.add("active");
+
+    setTimeout(() => {
+      keyElemento.classList.remove("active");
+    }, 300);
+  }
+}
