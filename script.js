74a2a6b0d40b08522de1cc4fc7b2adb304e0defa
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-09-30 16:10:24 -0300

    Criando função para reproduzir composição e adicionando efeito na tela.

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ document.body.addEventListener("keyup", (event) => {
   playSound(event.code.toLowerCase());
 });
 
+document.querySelector(".composer button").addEventListener("click", () => {
+  let song = document.querySelector("#input").value;
+
+  if (song !== "") {
+    let songArray = song.split("");
+    playComposition(songArray);
+  }
+});
+
 function playSound(sound) {
   let audioElement = document.querySelector(`#s_${sound}`);
   let keyElemento = document.querySelector(`div[data-key="${sound}"]`);
+  let body = document.body;
 
   if (audioElement) {
     audioElement.currentTime = 0;
@@ -13,9 +23,25 @@ function playSound(sound) {
 
   if (keyElemento) {
     keyElemento.classList.add("active");
+    body.style.backgroundColor = "rgba(128, 128, 128, 0.8)";
 
     setTimeout(() => {
       keyElemento.classList.remove("active");
     }, 300);
+
+    setTimeout(() => {
+      body.style.backgroundColor = "#333";
+    }, 100);
+  }
+}
+
+function playComposition(songArray) {
+  let wait = 0;
+
+  for (let songItem of songArray) {
+    setTimeout(() => {
+      playSound(`key${songItem}`);
+    }, wait);
+    wait += 250;
   }
 }
